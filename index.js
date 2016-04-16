552aa2c5867dcc1cb7a771f7f4f82078a83903bd
Jon Atkins <jon.atkins23@gmail.com>
2016-04-16 16:43:41 -0400

    no clue what was done but haven't committed in days

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express');
+
+var bodyParser = require('body-parser');
+var validator = require('validator');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/blueberry-cupcake-39701';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {]
+	db = databaseConnection;
+});
+
+// app.set('port', (process.env.PORT || 5000));
+
+//Serve static content
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -21,4 +34,15 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.post('/sendLocation', function(request, response) {
+	db.collection('locations', function(error, coll) {
+		var id = col.insert(toInsert, function(error, saved) {
+			if (error) {
+				response.send(500);
+			}
+			else {
+				response.send(200);
+			}
+		});
+	});
+});
